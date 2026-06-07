afb37ee99cfaffcf136be6a0f0698f2b0f275ea5
Surya Janardhan <121177774+SuryaJanardhan@users.noreply.github.com>
2026-06-07 22:13:12 +0530

    Time: 35 ms (61.87%) | Memory: 25.9 MB (27.41%) - LeetSync

--- 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.py
+++ 4289-valid-binary-strings-with-cost-limit/valid-binary-strings-with-cost-limit.py
@@ -1,25 +1,26 @@
 class Solution:
     def generateValidStrings(self, n: int, k: int) -> list[str]:
         bins = list()
-        def cost(s):
-            curr = 0
-            # s = list(s)
-            for i, ele in enumerate(s):
-                if ele == "1":
-                    curr += i
-                if curr >k:
-                    return k+1
-            return curr
+        # def cost(s):
+        #     curr = 0
+        #     # s = list(s)
+        #     for i, ele in enumerate(s):
+        #         if ele == "1":
+        #             curr += i
+        #         if curr >k:
+        #             return k+1
+        #     return curr
 
-        def generateStrings(s):
+        def generateStrings(s, cost):
 
+            if cost > k:
+                return
             if len(s) == n:
-                if cost(s) <= k:
-                    bins.append(s)
+                bins.append(s)
                 return 
-            generateStrings(s + "0")
+            generateStrings(s + "0", cost)
             if not s or s[-1] != "1":
-                generateStrings(s + "1")
-        generateStrings("")
+                generateStrings(s + "1", cost+len(s))
+        generateStrings("",0)
         # print(bins)
         return bins
