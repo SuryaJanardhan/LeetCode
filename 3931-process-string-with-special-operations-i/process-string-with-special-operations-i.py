eb659d5353254703370ff0f57677d638b364adc7
Surya Janardhan <121177774+SuryaJanardhan@users.noreply.github.com>
2026-06-16 18:16:51 +0530

    Time: 41 ms (49.34%) | Memory: 25.9 MB (44.08%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.py
@@ -0,0 +1,15 @@
+class Solution:
+    def processStr(self, s: str) -> str:
+        stack = []
+        
+        for i in s:
+            if i == '*':
+                if stack:
+                    stack.pop()
+            elif i == '#':
+                stack += stack
+            elif i == '%':
+                stack.reverse()
+            else:
+                stack.append(i)
+        return "".join(stack)
