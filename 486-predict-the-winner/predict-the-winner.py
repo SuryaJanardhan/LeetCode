a978d85356dfa5bdd1cf56c912c61b0a2ef66670
Surya Janardhan <121177774+SuryaJanardhan@users.noreply.github.com>
2026-08-01 14:09:29 +0530

    Time: 3323 ms (5.03%) | Memory: 18.8 MB (100.00%) - LeetSync

---
+++ 486-predict-the-winner/predict-the-winner.py
@@ -0,0 +1,21 @@
+class Solution:
+    def predictTheWinner(self, nums: List[int]) -> bool:
+        p1 = 0
+        p2 = 0
+        L , R = 0, len(nums)-1
+
+        def recur(p1, p2, L, R, T):
+
+            if L > R:
+                return p1>=p2
+
+            if T == 1:   
+                left = recur(p1 + nums[L], p2, L+1, R, 0)
+                right = recur(p1 + nums[R], p2, L, R-1, 0)
+                return left or right
+            else:           
+                left = recur(p1, p2 + nums[L], L+1, R, 1)
+                right = recur(p1, p2 + nums[R], L, R-1, 1)
+                return left and right
+
+        return recur(p1,p2,L, R ,1)
