d3525b4a06314b4ad9c5b84b46e2c096bff8c819
Surya Janardhan <121177774+SuryaJanardhan@users.noreply.github.com>
2026-08-29 17:48:54 +0530

    Time: 4 ms (41.89%) | Memory: 20.5 MB (18.10%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,33 +1,13 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
 
-        hashmap = {}
-
-        # o(n)
-
+        seen = {}
 
         for i in range(len(nums)):
+            need = target - nums[i]
 
-            #i odu n times tirugutunnadu 
-
-
-            num = target - nums[i] 
+            if need in seen:
+                return [seen[need], i]
             
-             #2 7 11 15
-            # 2 in hashmap(2,3,4) o(1) 
-            if num in hashmap:
-
-                # {2:0,7:1}
-            
-                return [ hashmap[num] , i]
-            else:
-                hashmap[nums[i]] = i #2 = 0 hashmap pettuko
-
-            
-
-        
-                
-        
-        
-  
+            seen[nums[i]] = i
         
